528e201cfe8e5bdf101d697b1e60d0167c040fb3
Jose Gregorio Materan <josemateran1998@gmail.com>
2022-07-11 17:46:27 -0400

    refactor: way to get the privateKey changed

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,10 +1,12 @@
 import admin from 'firebase-admin'
 
+const { privateKey } = JSON.parse(process.env.FIREBASE_PRIVATE_KEY as string)
+
 if (admin.apps.length === 0) {
   admin.initializeApp({
     credential: admin.credential.cert({
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY,
+      privateKey,
       projectId: process.env.FIREBASE_PROJECT_ID
     }),
     databaseURL: 'https://thegreg-example.firebaseio.com'
